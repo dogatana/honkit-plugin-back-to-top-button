149ac2a8beb28dffa4107f561e2a24f7f52d1ea7
Toshihiko Ichida <dogatana@gmail.com>
2020-09-25 08:43:53 +0900

    add configuration for the button glyph

--- assets/plugin.js
+++ assets/plugin.js
@@ -1,25 +1,31 @@
-var gitbook = window.gitbook;
+let button_html;
 
-gitbook.events.on('page.change', function() {
-	
-	var back_to_top_button = ['<div class="back-to-top"><i class="fa fa-arrow-up"></i></div>'].join("");
-	$(".book").append(back_to_top_button)
+// create button html at start
+window.gitbook.events.bind('start', function(e, config) {
+    const plugin = 'back-to-top-button';
+    let icon = '<i class="fa fa-arrow-up"></i>';
+    if (config[plugin] && config[plugin]['icon']) {
+        icon = config[plugin]['icon'];
+    }
+    button_html = `<div class="back-to-top">${icon}</i></div>`;
+});
 
-	$(".back-to-top").hide();
+window.gitbook.events.on('page.change', function() {
+    $(".book").append(button_html);
+    $(".back-to-top").hide();
 
 	$('.book-body,.body-inner').on('scroll', function () {
-		if ($(this).scrollTop() > 100) { 
+		if ($(this).scrollTop() > 100) {
 			$('.back-to-top').fadeIn();
 		} else {
 			$('.back-to-top').fadeOut();
 		}
 	});
 
-	$('.back-to-top').click(function () { 
+	$('.back-to-top').click(function () {
 		$('.book-body,.body-inner').animate({
 			scrollTop: 0
 		}, 800);
 		return false;
 	});
-	
 });
